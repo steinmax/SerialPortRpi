661fe57ffabe0edbb8ef3db17ab08ed8e39c5b64
steinmax <siegl.maxi@gmail.com>
2020-09-19 17:39:33 +0200

    Added New Message Form

---
+++ SerialPortRpi/frmNewMessage.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SerialPortRpi
+{
+    partial class frmNewMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmNewMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 481);
+            this.Name = "frmNewMessage";
+            this.Text = "frmNewMessage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SerialPortRpi/frmNewMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPortRpi
+{
+    public partial class frmNewMessage : Form
+    {
+        public frmNewMessage()
+        {
+            InitializeComponent();
+        }
+    }
+}
