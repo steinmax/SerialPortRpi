680986a226cbe17f1026b9fe6699a87f6b278bb8
steinmax <siegl.maxi@gmail.com>
2020-09-20 13:20:39 +0200

    Added a new form for numerical input

--- SerialPortRpi/Form1.cs
+++ SerialPortRpi/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace SerialPortRpi
 {
     public partial class Form1 : Form
     {
+        string selectedCOMPort = "";
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +22,63 @@ namespace SerialPortRpi
         private void panel2_MouseClick(object sender, MouseEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        Timer WatchUpdateTimer;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WatchUpdateTimer = new Timer();
+            WatchUpdateTimer.Interval = 500; //500ms
+            WatchUpdateTimer.Tick += WatchUpdateTimer_Tick;
+            WatchUpdateTimer.Enabled = true;
+
+            UpDateSerialPorts();
+        }
+
+        private void UpDateSerialPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            lbSerialPorts.Items.Clear();
+            lbSerialPorts.Items.AddRange(ports);
+        }
+
+        private void WatchUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lbnTime.Text = DateTime.Now.ToShortTimeString();
+            });
+            
+        }
+
+        private void label4_MouseClick(object sender, MouseEventArgs e)
+        {
+            frmNumInput newBaudMaker = new frmNumInput(serialPort.BaudRate);
+            newBaudMaker.ShowDialog();
+            if (newBaudMaker.isValid)
+            {
+                serialPort.BaudRate = newBaudMaker.BaudRate;
+                label4.Text = serialPort.BaudRate.ToString();
+            }
+        }
+
+        private void lbSerialPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lbSerialPorts.SelectedIndex;
+            if(index != -1)
+            {
+                //Valid Selcetion
+                gpConnection.Visible = true;
+                selectedCOMPort = lbSerialPorts.Items[index].ToString();
+                btnConnect.Text = $"Connect to {selectedCOMPort}";
+                
+            }
+        }
+
+        private void btnNewMessage_Click(object sender, EventArgs e)
+        {
+            frmNewMessage newMsgMaker = new frmNewMessage();
+            newMsgMaker.Show();
         }
     }
 }
